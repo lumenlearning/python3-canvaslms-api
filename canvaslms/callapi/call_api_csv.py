b5ae306cd6c71d0dda448ad33e1e522578099380
Aaron Johnson <aaron@lumenlearning.com>
2013-03-21 15:40:45 -0600

    Fix bug in call_api_csv to handle results with different field sets.
    canvaslms/callapi/call_api_csv.py
     - Clarify code to indicate which modules are used.
     - Use an OrderedDict as an ordered set to create a list of all the
       different field names that are used in the result objects.  When
       outputting to CSV, if the object doesn't have a field name that
       others had, give a None value.

--- canvaslms/callapi/call_api_csv.py
+++ canvaslms/callapi/call_api_csv.py
@@ -20,37 +20,48 @@
 # along with python3-canvaslms-api. If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
+import collections
+import csv
 import io
-from sys import argv, stderr, stdout
-from csv import DictWriter
+import sys
 
-from canvaslms.api import CanvasAPI, getAuthTokenFromFile
+import canvaslms.api as api
 
 def main():
-    if len(argv) == 1:
+    if len(sys.argv) == 1:
         printUsage()
         return
         
-    tokenFilePath = argv[1]
-    server = argv[2]
-    apiEndpoint = argv[3]
+    tokenFilePath = sys.argv[1]
+    server = sys.argv[2]
+    apiEndpoint = sys.argv[3]
 
     csv = call_api_csv(tokenFilePath, server, apiEndpoint)
 
     print(csv)
 
 def call_api_csv(tokenFilePath, server, apiEndpoint):
-    token = getAuthTokenFromFile(tokenFilePath)
-    api = CanvasAPI(defaultServer=server, defaultAuthToken=token)
+    token = api.getAuthTokenFromFile(tokenFilePath)
+    apiObj = api.CanvasAPI(defaultServer=server, defaultAuthToken=token)
 
-    result = api.allPages(apiEndpoint)
+    result = apiObj.allPages(apiEndpoint)
     if len(result) > 0:
-        headers = list(result[0].keys())
+        # allKeys is acting simply as an ordered set here.  Not all
+        #   of the results have the same set of data, which means that
+        #   we need to look at the headers for ALL of the results instead
+        #   of only the first one.
+        allKeys = collections.OrderedDict()
+        for r in result:
+            ks = list(r.keys())
+            for k in ks:
+                allKeys[k] = 1
+
+        headers = list(allKeys.keys())
     else:
         headers = []
 
     buf = io.StringIO()
-    dw = DictWriter(buf, headers)
+    dw = csv.DictWriter(buf, headers)
     dw.writeheader()
     dw.writerows(result)
 
